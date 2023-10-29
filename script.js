a1686a62241c5efd240324e60626e933acbdee9a
umer ijaz <46081877+taizguy@users.noreply.github.com>
2023-10-29 12:33:17 +0500

    new changes

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector('#main1'),
   smooth: true,
 });
+
+Shery.mouseFollower({
+  //Parameters are optional.
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
